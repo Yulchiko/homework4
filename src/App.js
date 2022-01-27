9fc371cc45c8688153311f5fe30bd4526ec11b2f
Yulchiko <Yulchik.82@i.ua>
2022-01-27 15:31:15 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {useReducer} from "react";
+
+const reducer = (state, action) => {
+    state = {...state, count2:state.count2 + 2}
+    state = {...state, count3:state.count3 + 5}
+
+    switch (action.type) {
+        case 'inc':
+            return {...state, count1: state.count1 + 1}
+        case 'dec':
+            return {...state, count1: state.count1 - 1}
+        case 'reset':
+            return {...state, count1: action.payload}
+        default:
+            throw new Error('Error')
+    }
+
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, dispatch] = useReducer(reducer, {count1: 0, count2: 0, count3: 0});
+
+    return (
+        <div>
+            <div className="Count">{state.count1}</div>
+            <div className="Count">{state.count2}</div>
+            <div className="Count">{state.count3}</div>
+            <button className="Button" onClick={() => dispatch({type: 'inc'})}>INC</button>
+            <button className="Button" onClick={() => dispatch({type: 'dec'})}>DEC</button>
+            <button className="Button" onClick={() => dispatch({type: 'reset', payload: 2})}>RESET</button>
+        </div>
+    )
 }
 
 export default App;
